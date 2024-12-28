f7935767f33583309e63d8eac2e6939922612eb2
Wangzehao2009 <wangzehaosimba@gmail.com>
2024-12-29 01:08:47 +0800

    upd install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,2 @@
-sudo npm init -y
-sudo npm install express mysql2 bcrypt multer parse fs cookie-parser marked ejs
-npm install marked
-npm install highlight.js
-sudo npm install axios
+npm install express mysql2 bcrypt multer parse fs cookie-parser ejs install highlight.js install axios
+brew install pandoc
