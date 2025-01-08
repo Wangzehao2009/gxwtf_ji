dd7095dcdb8babfdd95a6c40056153a4495a7bcc
Wangzehao2009 <wangzehaosimba@gmail.com>
2025-01-08 18:15:23 +0800

    upd mysql

--- guangfang.sql
+++ guangfang.sql
@@ -11,7 +11,7 @@
  Target Server Version : 90001 (9.0.1)
  File Encoding         : 65001
 
- Date: 03/01/2025 22:49:25
+ Date: 08/01/2025 18:15:10
 */
 
 SET NAMES utf8mb4;
@@ -121,6 +121,11 @@ INSERT INTO `issue_problem_graph` (`issue_id`, `problem_id`) VALUES (111, 45);
 INSERT INTO `issue_problem_graph` (`issue_id`, `problem_id`) VALUES (111, 46);
 INSERT INTO `issue_problem_graph` (`issue_id`, `problem_id`) VALUES (111, 48);
 INSERT INTO `issue_problem_graph` (`issue_id`, `problem_id`) VALUES (111, 49);
+INSERT INTO `issue_problem_graph` (`issue_id`, `problem_id`) VALUES (115, 50);
+INSERT INTO `issue_problem_graph` (`issue_id`, `problem_id`) VALUES (115, 51);
+INSERT INTO `issue_problem_graph` (`issue_id`, `problem_id`) VALUES (115, 52);
+INSERT INTO `issue_problem_graph` (`issue_id`, `problem_id`) VALUES (115, 53);
+INSERT INTO `issue_problem_graph` (`issue_id`, `problem_id`) VALUES (115, 54);
 COMMIT;
 
 -- ----------------------------
@@ -132,7 +137,7 @@ CREATE TABLE `issues` (
   `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=112 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=116 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of issues
@@ -142,6 +147,7 @@ INSERT INTO `issues` (`id`, `time`, `name`) VALUES (101, '2024-12-25 21:57:19', 
 INSERT INTO `issues` (`id`, `time`, `name`) VALUES (102, '2024-12-25 22:05:33', '第二期');
 INSERT INTO `issues` (`id`, `time`, `name`) VALUES (108, '2024-12-31 16:01:15', '第三期');
 INSERT INTO `issues` (`id`, `time`, `name`) VALUES (111, '2025-01-02 23:01:47', '第四期');
+INSERT INTO `issues` (`id`, `time`, `name`) VALUES (115, '2025-01-06 20:22:31', '第五期');
 COMMIT;
 
 -- ----------------------------
@@ -158,32 +164,42 @@ CREATE TABLE `problems` (
   `author` varchar(255) DEFAULT NULL,
   `file_path` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of problems
 -- ----------------------------
 BEGIN;
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (26, '2024-12-25 21:51:22', '第一期数学', '数学', NULL, NULL, '郑凯文', 'uploads/1735134682747-undefined-problem_1.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (27, '2024-12-25 21:52:40', '第一期物理', '物理', 0, NULL, '姜川', 'uploads/1735134760918-undefined-problem_27.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (28, '2024-12-25 21:54:05', '第一期化学', '化学', 1, NULL, '张天泽', 'uploads/1735134845494-undefined-problem_28.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (29, '2024-12-25 21:55:03', '第一期生物', '生物', NULL, NULL, '刘润青', 'uploads/1735134903216-undefined-problem_29.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (30, '2024-12-25 21:55:54', '第一期信息', '信息', NULL, NULL, '陈雨翔、王泽昊', 'uploads/1735134954188-undefined-problem_30.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (26, '2024-12-25 21:51:22', '第一期数学', '数学', 0, NULL, '郑凯文', 'uploads/1735134682747-undefined-problem_1.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (27, '2024-12-25 21:52:40', '第一期物理', '物理', 1, NULL, '姜川', 'uploads/1735134760918-undefined-problem_27.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (28, '2024-12-25 21:54:05', '第一期化学', '化学', 2, NULL, '张天泽', 'uploads/1735134845494-undefined-problem_28.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (29, '2024-12-25 21:55:03', '第一期生物', '生物', 0, NULL, '刘润青', 'uploads/1735134903216-undefined-problem_29.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (30, '2024-12-25 21:55:54', '第一期信息', '信息', 0, NULL, '陈雨翔、王泽昊', 'uploads/1735134954188-undefined-problem_30.md');
 INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (31, '2024-12-25 22:01:36', '第二期数学', '数学', 1, NULL, '姜傲博', 'uploads/1735135296420-undefined-problem_31.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (32, '2024-12-25 22:02:26', '第二期物理', '物理', NULL, NULL, '王思筝', 'uploads/1735135346727-undefined-problem_32.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (33, '2024-12-25 22:03:15', '第二期化学', '化学', NULL, NULL, '张汉祥', 'uploads/1735135395796-undefined-problem_33.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (35, '2024-12-25 22:04:22', '第二期生物', '生物', NULL, NULL, '刘润青', 'uploads/1735135462864-undefined-problem_35.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (32, '2024-12-25 22:02:26', '第二期物理', '物理', 0, NULL, '王思筝', 'uploads/1735135346727-undefined-problem_32.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (33, '2024-12-25 22:03:15', '第二期化学', '化学', 0, NULL, '张汉祥', 'uploads/1735135395796-undefined-problem_33.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (35, '2024-12-25 22:04:22', '第二期生物', '生物', 0, NULL, '刘润青', 'uploads/1735135462864-undefined-problem_35.md');
 INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (36, '2024-12-25 22:05:18', '第二期信息', '信息', 1, NULL, '施云浩', 'uploads/1735135518260-undefined-problem_36.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (37, '2024-12-29 21:29:26', '第三期数学', '数学', NULL, NULL, '郑凯文', 'uploads/1735478966904-undefined-problem_37.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (38, '2024-12-29 21:31:04', '第三期物理', '物理', NULL, NULL, '陶安泊', 'uploads/1735479064982-undefined-problem_38.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (39, '2024-12-29 21:31:50', '第三期化学', '化学', NULL, NULL, '张天泽', 'uploads/1735479110713-undefined-problem_39.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (41, '2024-12-29 21:34:46', '第三期生物', '生物', NULL, NULL, '廖欣妍', 'uploads/1735479286551-undefined-problem_41.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (42, '2024-12-29 21:36:26', '第三期信息', '信息', NULL, NULL, '陈雨翔、lky先生', 'uploads/1735479386335-undefined-problem_42.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (44, '2025-01-02 22:54:02', '第四期数学', '数学', NULL, NULL, '张潇瀚', 'uploads/1735829642291-Wangzehao2009-problem_43.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (45, '2025-01-02 22:55:39', '一代战神：“功臣号”', '物理', NULL, NULL, '马涵裕', 'uploads/1735829739331-Wangzehao2009-problem_45.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (46, '2025-01-02 22:56:45', '第四期化学', '化学', NULL, NULL, '庞凌熙', 'uploads/1735829805684-Wangzehao2009-problem_46.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (48, '2025-01-02 23:00:31', '第四期生物', '生物', NULL, NULL, '郑正', 'uploads/1735830031102-Wangzehao2009-problem_47.md');
-INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (49, '2025-01-02 23:01:14', '第四期信息', '信息', NULL, NULL, '黄锦扬', 'uploads/1735830074219-Wangzehao2009-problem_49.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (37, '2024-12-29 21:29:26', '第三期数学', '数学', 2, NULL, '郑凯文', 'uploads/1735478966904-undefined-problem_37.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (38, '2024-12-29 21:31:04', '第三期物理', '物理', 0, NULL, '陶安泊', 'uploads/1735479064982-undefined-problem_38.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (39, '2024-12-29 21:31:50', '第三期化学', '化学', 0, NULL, '张天泽', 'uploads/1735479110713-undefined-problem_39.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (41, '2024-12-29 21:34:46', '第三期生物', '生物', 1, NULL, '廖欣妍', 'uploads/1735479286551-undefined-problem_41.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (42, '2024-12-29 21:36:26', '第三期信息', '信息', 1, NULL, '陈雨翔、lky先生', 'uploads/1735479386335-undefined-problem_42.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (44, '2025-01-02 22:54:02', '第四期数学', '数学', 2, NULL, '张潇瀚', 'uploads/1735829642291-Wangzehao2009-problem_43.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (45, '2025-01-02 22:55:39', '一代战神：“功臣号”', '物理', 1, NULL, '马涵裕', 'uploads/1735829739331-Wangzehao2009-problem_45.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (46, '2025-01-02 22:56:45', '第四期化学', '化学', 1, NULL, '庞凌熙', 'uploads/1735829805684-Wangzehao2009-problem_46.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (48, '2025-01-02 23:00:31', '第四期生物', '生物', 1, NULL, '郑正', 'uploads/1735830031102-Wangzehao2009-problem_47.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (49, '2025-01-02 23:01:14', '第四期信息', '信息', 0, NULL, '黄锦扬', 'uploads/1735830074219-Wangzehao2009-problem_49.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (50, '2025-01-06 20:13:17', '第五期数学', '数学', 0, NULL, '张潇瀚', 'uploads/1736165597342-16-problem_50.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (51, '2025-01-06 20:14:26', 'jc 的水壶空了', '物理', 0, NULL, '段旭升', 'uploads/1736165666673-16-problem_51.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (52, '2025-01-06 20:14:45', '第五期化学', '化学', 0, NULL, '张鑫雨', 'uploads/1736165685781-16-problem_52.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (53, '2025-01-06 20:14:56', '米式方程', '生物', 0, NULL, '刘季涵', 'uploads/1736165696946-16-problem_53.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (54, '2025-01-06 20:15:12', '矩形加矩形求和', '信息', 0, NULL, '王泽昊', 'uploads/1736165712438-16-problem_54.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (65, '2025-01-07 21:47:25', '第六期数学', '数学', NULL, NULL, '张潇瀚', 'uploads/problem_55.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (66, '2025-01-07 21:48:00', '第六期物理', '物理', NULL, NULL, '张宸睿', 'uploads/problem_66.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (67, '2025-01-07 21:48:53', '第六期化学', '化学', NULL, NULL, '余东耕', 'uploads/problem_67.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (68, '2025-01-07 21:53:06', '第六期生物', '生物', NULL, NULL, '刘润清', 'uploads/problem_68.md');
+INSERT INTO `problems` (`id`, `time`, `name`, `subject`, `submit_num`, `score`, `author`, `file_path`) VALUES (69, '2025-01-07 21:56:27', '开胃菜', '信息', NULL, NULL, '董一瑄、李星成', 'uploads/problem_69.md');
 COMMIT;
 
 -- ----------------------------
@@ -198,16 +214,28 @@ CREATE TABLE `submissions` (
   `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `subject` varchar(255) DEFAULT NULL,
   `file_path` varchar(255) DEFAULT NULL,
+  `is_excel_answer` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of submissions
 -- ----------------------------
 BEGIN;
-INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`) VALUES (9, 16, 102, 31, '2024-12-31 13:54:21', '数学', 'uploads/1735624461168-16-image-1.png');
-INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`) VALUES (10, 16, 101, 28, '2024-12-31 13:54:34', '化学', 'uploads/1735624474292-16-reply.pdf');
-INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`) VALUES (11, 16, 102, 36, '2024-12-31 22:04:05', '信息', 'uploads/1735653845691-16-oi.md');
+INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`, `is_excel_answer`) VALUES (9, 16, 102, 31, '2024-12-31 13:54:21', '数学', 'uploads/1735624461168-16-image-1.png', 0);
+INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`, `is_excel_answer`) VALUES (10, 16, 101, 28, '2024-12-31 13:54:34', '化学', 'uploads/1735624474292-16-reply.pdf', 0);
+INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`, `is_excel_answer`) VALUES (11, 16, 102, 36, '2024-12-31 22:04:05', '信息', 'uploads/1735653845691-16-oi.md', 0);
+INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`, `is_excel_answer`) VALUES (12, 16, 111, 44, '2025-01-04 19:40:15', '数学', 'uploads/1735990815244-16-[Sqrt5+Round+2]+题解.pdf', 0);
+INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`, `is_excel_answer`) VALUES (13, 16, 111, 48, '2025-01-05 23:04:03', '生物', 'uploads/1736089443490-16-0506-WuChengKun.pdf', 1);
+INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`, `is_excel_answer`) VALUES (14, 16, 101, 27, '2025-01-06 18:39:03', '物理', 'uploads/1736159943714-16-[Sqrt5+Round+2]+题解.pdf', 0);
+INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`, `is_excel_answer`) VALUES (15, 17, 111, 45, '2025-01-06 19:10:36', '物理', 'uploads/1736161836872-17-2_17_3a549f5a704ffef73694103eaecd416.png', 0);
+INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`, `is_excel_answer`) VALUES (16, 17, 111, 46, '2025-01-06 19:52:21', '化学', 'uploads/1736164341399-17-tree1.in', 0);
+INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`, `is_excel_answer`) VALUES (17, 17, 108, 42, '2025-01-06 19:52:40', '信息', 'uploads/1736164360635-17-17_17_五题week2OI.pdf', 1);
+INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`, `is_excel_answer`) VALUES (18, 17, 108, 41, '2025-01-06 19:53:22', '生物', 'uploads/1736164402360-17-6_17_五题week3OI.pdf', 1);
+INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`, `is_excel_answer`) VALUES (19, 17, 108, 37, '2025-01-06 19:53:42', '数学', 'uploads/1736164422380-17-[Round+2]+solution', 0);
+INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`, `is_excel_answer`) VALUES (20, 18, 111, 44, '2025-01-06 20:01:37', '数学', 'uploads/1736164897261-18-题目得分率对比.png', 1);
+INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`, `is_excel_answer`) VALUES (23, 18, 108, 37, '2025-01-06 20:05:40', '数学', 'uploads/1736165140006-18-2_17_3a549f5a704ffef73694103eaecd416.png', NULL);
+INSERT INTO `submissions` (`id`, `user_id`, `issue_id`, `problem_id`, `time`, `subject`, `file_path`, `is_excel_answer`) VALUES (24, 18, 101, 28, '2025-01-06 20:08:50', '化学', 'uploads/1736165330799-18-[Sqrt5+Round+2]+题解.pdf', NULL);
 COMMIT;
 
 -- ----------------------------
@@ -225,7 +253,7 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of users
@@ -233,6 +261,8 @@ CREATE TABLE `users` (
 BEGIN;
 INSERT INTO `users` (`id`, `username`, `real_name`, `school`, `grade`, `phone`, `email`, `password`) VALUES (14, 'jzq', '季子谦', '北京师范大学附属中学', '9', '110', '114@514.com', '$2b$10$G4844T68NUJzyj0j87P/tO0bl2bKse260gFJ7VnQADeL4NBEgoyTK');
 INSERT INTO `users` (`id`, `username`, `real_name`, `school`, `grade`, `phone`, `email`, `password`) VALUES (16, 'Wangzehao2009', '王泽昊', '北京师范大学附属实验中学', '10', '15101533710', '2896653446@qq.com', '$2b$10$jV1FOAki5siBZTt1D8W3a.Mjmx1vBrNduROrUVL3qVwMu.j/aWZe2');
+INSERT INTO `users` (`id`, `username`, `real_name`, `school`, `grade`, `phone`, `email`, `password`) VALUES (17, 'bot1', 'bot1', '北京师范大学附属实验中学', '1', '1', '111@1.com', '$2b$10$WoXnsW/paHlRmc0/ZuoDH.vVx7KDD1HqADrRihkPRdpFJFYfTG7EG');
+INSERT INTO `users` (`id`, `username`, `real_name`, `school`, `grade`, `phone`, `email`, `password`) VALUES (18, 'bot2', 'bot2', '1', '1', '1', '1212@111.com', '$2b$10$.BM.n56xjIah1d2bRrRonu0zN7XaE..0rHUKLmERkcT7HEAlVNN4e');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
