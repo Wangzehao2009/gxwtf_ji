651e027839076a64557750ab30f812d223ed5050
Wangzehao2009 <wangzehaosimba@gmail.com>
2025-01-09 13:11:50 +0800

    upd banner

--- mysql.js
+++ mysql.js
@@ -3,7 +3,7 @@ const mysql=require('mysql2');
 const db=mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'jzq20100505',
+    password:'',
     database:'guangfang',
     dateStrings:true
 });
