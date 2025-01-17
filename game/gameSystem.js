d7d5433766fcf36924d1bb832ba66337b09438c6
Wangzehao2009 <wangzehaosimba@gmail.com>
2025-01-17 23:14:07 +0800

    1146

--- game/gameSystem.js
+++ game/gameSystem.js
@@ -3,8 +3,9 @@ const db = require('../mysql');
 function updateGameScore(req,res)
 {
     const {userId,add,type} = req.query;
+    if(!userId) return req.status(500).json({ message: '未登录'});
     db.query(
-        `UPDATE users set ${type}=${type}+${add} where id=${userId}`,
+        `UPDATE users SET ${type}=${type}+${add} WHERE id=${userId}`,
         (err,result2)=>{
             if(err){
                 console.error('数据库错误', err);
